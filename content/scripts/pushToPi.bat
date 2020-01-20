1897abf24200365dd4fd33d66b725f892ff31a7e
erikest <erikest@gmail.com>
2020-01-19 20:39:58 -0800

    updated package, cleaned up push to pi script and template.json

--- content/scripts/pushToPi.bat
+++ content/scripts/pushToPi.bat
@@ -1,3 +1,3 @@
-dotnet publish ../gRPCpi/src/gRPCpi.Client/gRPCpi.Client.csproj -r linux-arm 
+dotnet publish ../src/gRPCpi.Client/gRPCpi.Client.csproj -r linux-arm 
 
-scp -rp ../gRPCpi/src/gRPCpi.Client/bin/Debug/netcoreapp3.0/linux-arm/publish/ pi@192.168.1.180:~/gRPCpi_Client
+scp -rp ../src/gRPCpi.Client/bin/Debug/netcoreapp3.1/linux-arm/publish/ pi@192.168.1.180:~/gRPCpi_Client
